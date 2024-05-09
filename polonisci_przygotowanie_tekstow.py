62c897cb23656131c4ffd1a13bdfe1d52b320cf2
Cezary Rosiński <cezary.rosinski@gmail.com>
2024-05-09 15:58:37 +0200

    data for PH

--- polonisci_przygotowanie_tekstow.py
+++ polonisci_przygotowanie_tekstow.py
@@ -3,6 +3,8 @@ sys.path.insert(1, 'C:/Users/Cezary/Documents/IBL-PAN-Python')
 from my_functions import gsheet_to_df
 from glob import glob
 import regex as re
+from tqdm import tqdm
+import pickle
 
 #%%
 
@@ -13,3 +15,29 @@ abstract_files = [f for f in glob(f"{path}*", recursive=True)]
 
 path = r"C:\Users\Cezary\Documents\polonisci\data\bibliotekanauki\txt\text_pl/"
 text_files = [f for f in glob(f"{path}*", recursive=True)]
+
+polonisci_db = polonisci_db.loc[polonisci_db['is_polish_scientist'] == 'tak']
+text_ids = [e.split(':')[-1] for e in polonisci_db['id'].to_list()]
+
+abstract_files_ok = [e for e in abstract_files if e.split('\\')[-1].replace('.txt', '') in text_ids]
+text_files_ok = [e for e in text_files if e.split('\\')[-1].replace('.txt', '') in text_ids]
+
+text_files_content = {}
+for abstract in tqdm(abstract_files_ok):
+    with open(abstract, encoding='utf-8') as f:
+        x = f.read()
+        text_files_content.update({abstract.split('\\')[-1].replace('.txt', ''): {'abstract': x}})
+        
+for text in tqdm(text_files_ok):
+    with open(text, encoding='utf-8') as f:
+        x = f.read()
+        text_id = text.split('\\')[-1].replace('.txt', '')
+        if text_id in text_files_content:
+            text_files_content[text_id].update({'full text': x})
+        else: text_files_content.update({text_id: {'full text': x}})
+        
+with open('data/polonisci_text_abstract.pickle', 'wb') as handle:
+    pickle.dump(text_files_content, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
+# with open('filename.pickle', 'rb') as handle:
+#     b = pickle.load(handle)
