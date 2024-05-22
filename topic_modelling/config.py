699dce5865936e0a6a33cfa074fb99bd44497e62
Patryk Hubar <38289522+patthub@users.noreply.github.com>
2024-05-22 16:18:40 +0200

    Create config.py

---
+++ topic_modelling/config.py
@@ -0,0 +1,7 @@
+POLISH_STOPWORDS = [
+    'i', 'oraz', 'a', 'ale', 'w', 'na', 'z', 'do', 'się', 'o', 'że', 'to', 'co', 'nie', 'jest', 'jak', 'tak', 'czy',
+    'być', 'by', 'dla', 'czyli', 'ten', 'ona', 'on', 'my', 'oni', 'jeśli', 'gdy', 'za', 'tylko', 'po', 'przez', 'pod',
+    'bez', 'nad', 'jeszcze', 'już', 'też', 'tam', 'tu', 'u', 'od', 'ze', 'jestem', 'są', 'jesteśmy', 'jesteś', 'was',
+    'czyż', 'albo', 'mnie', 'tobie', 'tobą', 'wam', 'może', 'muszę', 'mogę', 'będzie', 'będą', 'był', 'była', 'było',
+    'byli', 'były', 'mam', 'masz', 'mamy', 'macie', 'ich', 'je', 'jemu', 'jej', 'jego', 'ich', 'imię', 'im'
+]
